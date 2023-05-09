b7cf611b9f26629a3cb3f50007bc58ccfe1ef693
KariraAnindya <133017246+KariraAnindya@users.noreply.github.com>
2023-05-09 23:55:24 +0700

    Add files via upload

--- cod.py
+++ cod.py
@@ -6,17 +6,17 @@ st.set_page_config(page_title='Perhitungan Kadar COD', page_icon=':memo:', layou
 st.title('Perhitungan Kadar COD')
 
 st.write('''
-    "Hallo! Website ini dibuat untuk memudahkan kamu dalam menghitung kadar COD sebelum melakukan analisa. Website ini sangat membantu dalam pengolahan data jika menjadi sebuah alat yang sangat berkembang pesat. Perkembangan ini membawa dampak positif bagi kampus yang berkembang"''')
+    Hallo! Website ini dibuat untuk memudahkan kamu dalam menghitung kadar COD sebelum melakukan analisa. Website ini sangat membantu dalam pengolahan data jika menjadi sebuah alat yang sangat berkembang pesat. Perkembangan ini membawa dampak positif bagi kampus yang berkembang''')
 
 st.write('''
-    "Chemical Oxygen Demand (COD) adalah metode analisis kimia yang digunakan untuk mengukur jumlah bahan organik yang teroksidasi dalam sampel air atau limbah cair. Metode ini digunakan sebagai indikator kualitas air untuk menentukan sejauh mana air tersebut tercemar oleh bahan-bahan organik terlarut.
+    Chemical Oxygen Demand (COD) adalah metode analisis kimia yang digunakan untuk mengukur jumlah bahan organik yang teroksidasi dalam sampel air atau limbah cair. Metode ini digunakan sebagai indikator kualitas air untuk menentukan sejauh mana air tersebut tercemar oleh bahan-bahan organik terlarut.
 
 Pada metode COD, bahan organik dalam sampel dioksidasi dengan menggunakan larutan oksidator kuat seperti kalium dikromat (K2Cr2O7) dalam kondisi asam. Proses oksidasi ini akan menghasilkan senyawa-senyawa seperti karbon dioksida, air, dan senyawa-senyawa anorganik lainnya.
 
 Kemudian, jumlah oksigen yang dibutuhkan untuk proses oksidasi ini diukur dan dinyatakan dalam satuan ppm (part per million) atau mg/L (miligram per liter). Semakin tinggi nilai COD dalam sampel, semakin banyak bahan organik teroksidasi, yang menunjukkan bahwa kualitas air semakin buruk atau tercemar.
 
 Metode COD sering digunakan dalam pengujian kualitas air limbah, industri, dan lingkungan. Nilai COD dapat digunakan sebagai indikator awal untuk menentukan proses pengolahan air yang dibutuhkan sebelum dibuang ke lingkungan atau digunakan kembali.
-"''')
+''')
 
 st.subheader('Kelompok 5')
 st.write ('''
@@ -34,7 +34,7 @@ def cod_calculator(app_mode):
         volume_blanko = st.number_input("Masukkan volume blanko (mL)")
         volume_pereaksi = st.number_input("Masukkan volume pereaksi (mL)")
         normalitas = st.number_input("Masukkan nilai normalitas (grek/mL)")
-        berat_ekivalen_oksigen = st.number_input("Masukkan berat ekivalen oksigen (mL) (Tetapan dalam SNI 8*1000)")
+        berat_ekivalen_oksigen = st.number_input("Masukkan berat ekivalen oksigen (mL) (Tetapan dalam SNI 8000)")
         volume_sampel = st.number_input("Masukkan nilai volume sampel (mL)")
 
         if volume_sampel !=0:

ed8e65fbc200ca0db0d682ec196b098c6a617482
KariraAnindya <133017246+KariraAnindya@users.noreply.github.com>
2023-05-09 23:00:48 +0700

    Add files via upload

--- cod.py
+++ cod.py
@@ -33,10 +33,12 @@ def cod_calculator(app_mode):
         berat_ekivalen_oksigen = st.number_input("Masukkan berat ekivalen oksigen (mL) (Tetapan dalam SNI 8*1000)")
         volume_sampel = st.number_input("Masukkan nilai volume sampel (mL)")
 
+    if volume_sampel !=0:
         cod = (volume_blanko - volume_pereaksi) * normalitas * berat_ekivalen_oksigen / volume_sampel
         st.write(f"The Chemical Oxygen Demand (COD) is {cod:.2f} mg/L")
-        return cod
-        
+    else :
+        st.write("Error: volume_sampel should not be 0.")
+
     elif app_mode == "Analysis COD":
         st.header("Analysis COD")
         st.write("Upload your COD analysis data in CSV format below")
